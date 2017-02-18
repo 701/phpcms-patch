70910f521e312bcb64665659ccf93ec3d9d1829f
2007-601@163.com <sdd-5581606>
2017-02-18 21:12:48 +0800

    *  漏洞: phpcms注入漏洞

--- phpcms/modules/poster/poster.php
+++ phpcms/modules/poster/poster.php
@@ -218,7 +218,7 @@ class poster extends admin {
         	$where .= " AND `type`='".$_GET['click']."'";
         	
         	//如果设置了按地区或者按ip分类
-	        if ($_GET['group']) {
+	        if ($_GET['group']) { $_GET['group'] = preg_replace('#`#', '', $_GET['group']);
 	        	$group = " `".$_GET['group']."`";
 	        	$fields = "*, COUNT(".$_GET['group'].") AS num";
 	        	$order = " `num` DESC";
